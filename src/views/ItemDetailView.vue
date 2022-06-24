5f03292e6e77883af8a54c3f98a462c1bb4089c7
zzee22su <patrickstar0910@gmail.com>
2022-06-24 17:57:18 +0900

    ADD: select tag에 v-model 적용

--- src/views/ItemDetailView.vue
+++ src/views/ItemDetailView.vue
@@ -9,50 +9,47 @@
                         <tbody>
                             <tr>
                                 <th scope="row">상품가</th>
-                                <td>{{ itemDetail.price }}</td>
+                                <td>{{ itemDetail.price+'원' }}</td>
                             </tr>
                             <tr>
                                 <th scope="row">배송비</th>
-                                <td>{{ itemDetail.deliveryCost }}</td>
+                                <td>{{ itemDetail.deliveryCost+'원' }}</td>
                             </tr>
                             <tr scope="row" v-for="options in itemDetail.productionOptions" :key="options.index">
                                 <th scopd="row">{{ options.name }}</th>
                                     <td >
-                                        <select>
-                                            <option v-for="types in options.optionType" :key="types.index" :value="types.price">{{ types.type }} {{ types.price+'원' }}</option>
+                                        <select v-model="test" @change="selectedOption">
+                                            <option selected>{{ defaultMsg }}</option>
+                                            <option v-for="types in options.optionType" :key="types.index" :value="types.type" >{{ types.type }} {{ '(+'+types.price+'원)' }}</option>
                                         </select>
                                     </td>
                             </tr>
+                            <!-- 옵션 선택 시, 표시 됌. -->
+                            <tr>
+                                <td>{{ itemDetail.name }}<br>
+                                <!-- TODO:  -->
+                                    {{ test }}
+                                <!-- TODO:  -->
+                                </td>
+                                <td>
+                                    <div class="d-grid gap-2 d-md-block">
+                                        <button class="btn btn-light" type="button">-</button>
+                                        <button class="btn btn-light" type="button">숫자표시</button>
+                                        <button class="btn btn-light" type="button">+</button>
+                                    </div>
+                                </td>
+                                 <td>18,000원</td>
+                            </tr>
                             <tr>
                                 <th scope="row">합계</th>
-                                <td>18,000원</td>
+                                <td>{{ totalPrice }}</td>
                             </tr>
                         </tbody>
-                        <div class="div-option">
-                            <!-- 옵션 선택 시, 표시 됌. -->
-                            <table class="table table-borderless">
-                                <tbody>
-                                    <tr>
-                                        <th scope="row">상품이름표기</th>
-                                        <td>18,000원</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">수량표시</th>
-                                        <td>
-                                            <div class="d-grid gap-2 d-md-block">
-                                                <button class="btn btn-light" type="button">-</button>
-                                                <button class="btn btn-light" type="button">숫자표시</button>
-                                                <button class="btn btn-light" type="button">+</button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">가격표시</th>
-                                        <td>18,000원</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+
+
+<div>
+</div>
+
                         <div class="d-grid gap-2">
                             <button class="btn btn-primary" type="button">바로구매</button>
                         </div>
@@ -70,6 +67,7 @@ import ProductAPI from '@/api/ProductAPI';
 export default {
     name: 'item-detail',
     data() {
+        console.log('data()');
         return {
             itemDetail: {
                 name: '', //상품명
@@ -80,12 +78,18 @@ export default {
             },
             defaultMsg:'[필수] 옵션을 선택해주세요.',
             isRequiredOption: false,
+            totalPrice: 0,
             test: ''
         }
     },
 
     created() {
+        console.log('created');
         this.getProductDetail(this.$route.params.itemId);
+    },
+
+    mounted() {
+         console.log('mounted'); 
     },
 
     methods: {
@@ -98,24 +102,16 @@ export default {
                 this.itemDetail.deliveryCost = itemData.deliveryCost;
                 this.itemDetail.point = itemData.point;
                 this.itemDetail.productionOptions = itemData.productionOptions;
-                console.log('옵션 값: '+JSON.stringify(this.itemDetail.productionOptions));
+                console.log(itemData);
             }
         },
 
-        requiredOption(event) {
-            console.log("필수 옵션 값 : "+event.target.value);
-            if(event.target.value === this.defaultMsg) {
-                this.isRequiredOption = false;
-                this.selectedOption(event);
-            } else {
-                this.isRequiredOption = true;
-            } 
-        },
-
         selectedOption(event) {
-            console.log("하위 옵션 값 : "+event.target.value);
-            if(event.target.value === this.defaultMsg) {
-                this.isRequiredOption = false;
+            console.log("옵션 값 : "+JSON.stringify(event.target));
+            if(event.target.value != this.defaultMsg) {
+                this.isRequiredOption = true;
+                this.totalPrice = this.itemDetail.price + parseInt(event.target.value)
+                console.log("합계 :"+this.totalPrice);
             }
         }
     }
